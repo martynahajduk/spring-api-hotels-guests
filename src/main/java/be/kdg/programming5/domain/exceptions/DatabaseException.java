b134b15f9620181aaddcd8ed57e09e11994ae79d
martyna.hajduk <martyna.hajduk@student.kdg.be>
2025-02-13 12:49:18 +0100

    Week 1, optimizing query count, fixing fetching

--- src/main/java/be/kdg/programming5/domain/exceptions/DatabaseException.java
+++ src/main/java/be/kdg/programming5/domain/exceptions/DatabaseException.java
@@ -1,11 +1,9 @@
 package be.kdg.programming5.domain.exceptions;
 
-//TODO
 public class DatabaseException extends RuntimeException {
     private DatabaseException(String message) {
         super(message);
     }
-
     public DatabaseException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/be/kdg/programming5/domain/exceptions/InvalidOperationException.java
+++ src/main/java/be/kdg/programming5/domain/exceptions/InvalidOperationException.java
@@ -3,7 +3,6 @@ package be.kdg.programming5.domain.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-//TODO
 @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid Hotel Operation")
 public class InvalidOperationException extends RuntimeException {
     private InvalidOperationException(String message) {
